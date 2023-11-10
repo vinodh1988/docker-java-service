1ba12c39477e0ff4f9c8786e9674343fef9a979c
vinodh1988 <k.c.vinodh@hotmail.com>
2023-11-10 22:51:10 +0530

    Update PeopleIntegrationTests.java

--- src/test/java/com/restapp/PeopleIntegrationTests.java
+++ src/test/java/com/restapp/PeopleIntegrationTests.java
@@ -15,29 +15,5 @@ import com.restapp.entities.Person;
 
 //@SpringBootTest(classes=RestMicroserviceApplication.class,webEnvironment = WebEnvironment.RANDOM_PORT)
 public class PeopleIntegrationTests {
-   @LocalServerPort
-   private int port;
-   
-   @Autowired TestRestTemplate restTemplate;
-   
-   @Test
-   public void testPeople()
-   {
-	 Person[] p= restTemplate.getForObject("http://localhost:"+port+"/api/db/people", Person[].class);
-	 
-	 
-	 assertThat(p[0].getName()).isEqualTo("Harry");
-	 assertThat(p.length).isEqualTo(10);
-	   
-   }
-   
-   @Test
-   public void testAddPerson() {
-	   Person p=new Person(10,"Sony","Mumbai");
-	   ResponseEntity<Person> response=
-			   this.restTemplate.postForEntity("http://localhost:"+port+"/api/db/people", p, Person.class);
-	   assertThat(response.getBody().getName()).isEqualTo("Sony");
-	   assertEquals(response.getStatusCodeValue(), 201);
-   }
-   
+  
 }
